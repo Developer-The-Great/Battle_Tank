0e180394070cd037d87a925ade9fd17ad8cb2e13
DESKTOP-AROIUH1\Raphael <blue.person16@gmail.com>
2018-08-14 19:44:18 +0700

    MainMenu

---
+++ Battle_Tank/Source/Battle_Tank/Tank_Player_Controller.h
@@ -0,0 +1,20 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/PlayerController.h"
+#include "Tank_Player_Controller.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class BATTLE_TANK_API ATank_Player_Controller : public APlayerController
+{
+	GENERATED_BODY()
+	
+	
+	
+	
+};
